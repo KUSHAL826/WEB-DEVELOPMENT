cd9ce6532ea3be0461226f316f8fb427e7e5af0b
Kushal <kushalyngowda@gmail.com>
2026-02-24 21:04:10 +0530

    Learning Basic JavaScript with some projects

---
+++ basicJavaScript/app.js
@@ -0,0 +1,38 @@
+// //alert("hello world")
+// //console.log(123);
+// //console.log("Hello");
+// let x='hello';
+// console.log(x);
+// //array
+// console.log([1,2,3]);
+// //KEY VALUE PAIR
+// console.log({a:1,b:2,c:3}); 
+// let abc=10.28;
+// console.log( typeof abc);
+// const date=new Date();
+// console.log(typeof date);
+// let val=new Date();
+// console.log(val);
+// let x=String(val);
+// console.log(x);
+// let x=String(Date());
+// console.log(x);
+// let x=5;
+// let y=x.toString();
+// console.log(y);
+// console.log(typeof y);
+// let x=5;
+// let z=String(x);
+// console.log(z);
+// console.log(typeof z);
+// let y=Number(z);
+// console.log(y);
+// console.log(typeof y);
+let fname=window.prompt("ENter username")
+console.log(fname);
+document.getElementById("myButton").onclick=function()
+{
+    let uname=document.getElementById("myText").value ;
+    console.log(uname);
+}
+let date=new Date();
